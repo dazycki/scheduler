ca79d8203e51c3325d7e50e6c56bef3b75788eb2
Daryan Zycki <daryan@Daryans-MBP.phub.net.cable.rogers.com>
2022-06-12 16:57:49 -0400

    Add DayList component to Application.js and refactor DayList to pass setDay and selected to DayListItem.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,12 +2,15 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+
   const days = props.days.map((day) => {
     return (
       <DayListItem
         key={day.id}
         name={day.name}
         spots={day.spots}
+        selected={props.day === day.name}
+        setDay={props.setDay}
       />
     );
   });
