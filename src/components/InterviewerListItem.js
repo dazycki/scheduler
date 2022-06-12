f9cbe4fa59753e141be08ac0a86b39b9bd75afb4
Daryan Zycki <daryan@Daryans-MBP.phub.net.cable.rogers.com>
2022-06-12 18:04:37 -0400

    Add InterviewerList component with new stories.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,7 +10,7 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li className={interviewerClass} onClick={() => setInterviewer(id)}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img className="interviewers__item-image" src={avatar} alt={name} />
       {selected && name}
     </li>
