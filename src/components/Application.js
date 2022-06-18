9db4ed7af7ab603bf11806138b7b551035a0cee9
Daryan Zycki <daryan@Daryans-MBP.phub.net.cable.rogers.com>
2022-06-18 14:20:47 -0400

    Fetch appointments from API and refactor to handle dynamic response.

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,64 +3,31 @@ import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
-
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // appointments: {},
+    appointments: {},
   });
-  const setDay = (day) => setState((prev) => ({ ...prev, day }));
-  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
-  const mapAppointments = Object.values(appointments).map((appointment) => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const setDay = (day) => setState((prev) => ({ ...prev, day }));
+
+  const mapAppointments = dailyAppointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
 
   useEffect(() => {
-    axios
-      .get("/api/days")
-      .then((response) => {
-        setDays(response.data);
+    Promise.all([axios.get("/api/days"), axios.get("/api/appointments")])
+      .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+        }));
       })
       .catch((err) => console.log(err.message));
   }, []);
