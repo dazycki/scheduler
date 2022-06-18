543beb68a092cb3a6a445e126e55205d8db8a483
Daryan Zycki <daryan@Daryans-MBP.phub.net.cable.rogers.com>
2022-06-18 13:23:40 -0400

    Remove hard coded days array, import axios, and fetch days from API server.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -62,9 +45,20 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
   const mapAppointments = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
+
+  useEffect(() => {
+    axios
+      .get("/api/days")
+      .then((response) => {
+        setDays(response.data);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
 
   return (
     <main className="layout">
