eb4cbce7ec95962126e69e7e0c1b45d48ef81b90
Daryan Zycki <daryan@Daryans-MBP.phub.net.cable.rogers.com>
2022-06-19 15:48:33 -0400

    Add more tests to Application componenet to increase code coverage.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -18,4 +18,24 @@ describe("Appointment", () => {
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
   });
+
+  it("changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
